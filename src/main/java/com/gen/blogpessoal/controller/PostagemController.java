8c70ad148409c72b154c31d933dfd10d5bcb435e
Jonathan <jonathan.ribeiro28121997@gmail.com>
2023-06-28 08:26:06 -0300

    Create Repository and controller the postagem

---
+++ src/main/java/com/gen/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,28 @@
+package com.gen.blogpessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gen.blogpessoal.model.Postagem;
+import com.gen.blogpessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	@GetMapping
+	private ResponseEntity<List<Postagem>> GettAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+
+}
---
+++ src/main/java/com/gen/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,10 @@
+package com.gen.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gen.blogpessoal.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+
+}
