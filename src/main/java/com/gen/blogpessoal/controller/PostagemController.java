555d86920f6bebff7c917ff3bc636d42c8b85ec2
Jonathan <jonathan.ribeiro28121997@gmail.com>
2023-06-28 08:58:17 -0300

    Created getByTitulo, getById, Post de postagem

--- src/main/java/com/gen/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/gen/blogpessoal/controller/PostagemController.java
@@ -3,14 +3,21 @@ package com.gen.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gen.blogpessoal.model.Postagem;
 import com.gen.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/postagens")
@@ -24,5 +31,22 @@ public class PostagemController {
 	private ResponseEntity<List<Postagem>> GettAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	private ResponseEntity<Postagem> GetById(@PathVariable Long id) {
+		return postagemRepository.findById(id).map(reposta -> ResponseEntity.ok(reposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
 
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> FindByTitulo (@PathVariable String titulo) {
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post (@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(postagemRepository.save(postagem));
+	}
+ 
 }
